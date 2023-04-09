88f857f68f51dc301dbf6ab75f11a1ef9c1cc586
李澔澤 <a0909007892@gmail.com>
2023-04-09 13:31:36 +0800

    PC registry end

---
+++ runtime_data_area_03/src/main/java/PCRegistryTest.java
@@ -0,0 +1,20 @@
+/**
+ * ProjectName: jvm_study
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @author a0909
+ * @version v1.0
+ * @create 2023/4/9 - 下午 01:03
+ * @since JDK 1.8
+ */
+public class PCRegistryTest {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+        String s = "abc";
+        System.out.println(i);
+        System.out.println(k);
+    }
+}
