f35fef10ceb499d21eeb4efbfe80a350eaffdce7
李澔澤 <a0909007892@gmail.com>
2023-07-14 21:02:10 +0800

    20230714 commit

---
+++ runtime_data_area_03/src/main/java/LocalVariableTest.java
@@ -0,0 +1,58 @@
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+/**
+ * ProjectName: jvm_study
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @author a0909
+ * @version v1.0
+ * @create 2023/7/14 - 下午 08:14
+ * @since JDK 1.8
+ */
+public class LocalVariableTest {
+
+    private int count = 0;
+
+    public static void main(String[] args) {
+        LocalVariableTest test = new LocalVariableTest();
+        int num = 10;
+        test.test1();
+    }
+
+    public static void testStatic(){
+        LocalVariableTest test = new LocalVariableTest();
+        Date date = new Date();
+        int count = 10;
+        System.out.println(count);
+    }
+
+    public void test1(){
+        Date date = new Date();
+        String name1 = "test";
+        String info = test2(date, name1);
+        System.out.println(date + name1);
+    }
+
+    public String test2(Date dateP, String name2){
+        dateP = null;
+        name2 = "test2";
+        double weight = 130.5;
+        char gender = '男';
+        return dateP + name2;
+    }
+
+    public void test3(){
+        count++;
+    }
+
+    public void test4(){
+        int a = 0;
+        {
+            int b = 0;
+            b = a+1;
+        }
+        int c = a + 1;
+    }
+}
